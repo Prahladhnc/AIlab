76a126747ac442223781c7ab05273732f049e9e7
Prahladhnc <116821694+Prahladhnc@users.noreply.github.com>
2023-07-31 07:06:09 +0530

    Add files via upload

---
+++ merge.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+int counts=0;
+
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int leftArray[n1], rightArray[n2];
+
+    for (int i = 0; i < n1; i++)
+        leftArray[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        rightArray[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (leftArray[i] <= rightArray[j]) {
+            arr[k] = leftArray[i];
+            i++;
+        } else {
+            arr[k] = rightArray[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = leftArray[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = rightArray[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    counts += 1;
+    if (left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+
+    merge(arr, left, mid, right);
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7, 9, 41, 22, 38, 10, 25, 33, 16, 27, 21};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Original array: ";
+    for (int i = 0; i < n; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+
+    mergeSort(arr, 0, n - 1);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+    std::cout << counts;
+    return 0;
+}
